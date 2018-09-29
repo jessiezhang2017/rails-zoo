2b9e2ee3eb4f8a31bc344207bfbd70afa4e84910
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-09-28 21:35:27 -0700

    add the edit page and link it from the show page

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -11,4 +11,37 @@ class AnimalsController < ApplicationController
      render :notfound, status: :not_found
     end
   end
+
+  def new
+    @task = Animal.new
+  end
+
+  def create
+    @task = Animal.new(animal_params)
+
+    if @animal.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @animal = Animal.find_by(id: params[:id].to_i)
+  end
+
+  def update
+    @animal = Animal.find_by(id: params[:id].to_i)
+
+    if @animal.update(animal_params)
+      redirect_to animal_path
+    else
+      render
+    end
+  end
+
+  private
+    def animal_params
+      return params.require(:animal).permit(:name, :species, :age)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   get "/animals", to: "animals#index", as: "animals"
 
+  get 'animals/new', to: 'animals#new', as: 'new_animal'
+  post 'animals', to: 'animals#create'
+
+  patch 'animals/:id', to: 'animals#update'
+  get '/animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
 
   get "/animals/:id", to: "animals#show", as: "animal"
 

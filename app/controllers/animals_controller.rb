51db3315e02174001d77ab901166f4737b3d0fef
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-09-28 20:58:16 -0700

    link the show page from the index page

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,4 +2,13 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all.order(:id)
   end
+
+  def show
+    id = params[:id].to_i
+    @animal = Animal.find_by(id: id)
+
+    if @animal.nil?
+     render :notfound, status: :not_found
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   get "/animals", to: "animals#index", as: "animals"
 
+
+  get "/animals/:id", to: "animals#show", as: "animal"
+
 end
